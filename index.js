f179f32ffc38c78120f0e9e41784f3c3f99e3c5c
Quentin Gliech <quentingliech@gmail.com>
2016-02-11 01:22:59 +0100

    Added TextareaAdaptor

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Editor from './lib/Editor';
 import CodeMirrorAdapter from './lib/CodeMirrorAdapter';
+import TextareaAdapter from './lib/TextareaAdapter';
 import GenericAdapter from './lib/GenericAdapter';
 
 module.exports = Editor;
 module.exports.CodeMirrorAdapter = CodeMirrorAdapter;
+module.exports.TextareaAdapter = TextareaAdapter;
 module.exports.GenericAdapter = GenericAdapter;
