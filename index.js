0cacfdac458346840d210005166ec07e0c3e1d35
Quentin Gliech <quentingliech@gmail.com>
2016-02-11 20:47:23 +0100

    Prepare to publish
    Move lib/* -> src/
    Use babel-cli to transpile es6 files
    Add LICENSE

--- index.js
+++
@@ -1,9 +0,0 @@
-import Editor from './lib/Editor';
-import CodeMirrorAdapter from './lib/CodeMirrorAdapter';
-import TextareaAdapter from './lib/TextareaAdapter';
-import GenericAdapter from './lib/GenericAdapter';
-
-module.exports = Editor;
-module.exports.CodeMirrorAdapter = CodeMirrorAdapter;
-module.exports.TextareaAdapter = TextareaAdapter;
-module.exports.GenericAdapter = GenericAdapter;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Editor from './Editor';
+import CodeMirrorAdapter from './CodeMirrorAdapter';
+import TextareaAdapter from './TextareaAdapter';
+import GenericAdapter from './GenericAdapter';
+
+module.exports = Editor;
+module.exports.CodeMirrorAdapter = CodeMirrorAdapter;
+module.exports.TextareaAdapter = TextareaAdapter;
+module.exports.GenericAdapter = GenericAdapter;
